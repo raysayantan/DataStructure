2c6b9222965ab4d5edf6c1640fa71aac79df2168
raysayantan <ray.sayantan@gmail.com>
2019-04-10 15:19:52 +0530

    Update CoinChange.java

--- DynamicProgramming/java/CoinChange.java
+++ DynamicProgramming/java/CoinChange.java
@@ -1,53 +1,38 @@
 
-public class LeveshteinDistance {
-
+public class CoinChange {
+	
+	static int [][] arr = new int[3][6];
+    static int [] dinno = {2,3,4};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		String s1 = "abcdef";
-		String s2 = "azced";
-		
-		System.out.println(getDistance(s1, s2));
+		System.out.println(getNumWays(5));
 
 	}
 	
-	public static int getDistance(String str1, String str2) {
-		int dist = 0;
-		int l2 = str1.length();
-		int l1 = str2.length();
-		
-		int [][] arr = new int[l1+1][l2+1];
-		
-		for(int i = 0; i <= l1; i++) {
-			for(int j = 0; j <= l2; j++) {
-				if(i == 0 && j ==0) {
-					arr[i][j] = 0;
-				} else if(i == 0) {
-					arr[i][j] = arr[i][j-1] + 1;
-				} else if (j == 0) {
-					arr[i][j] = arr[i-1][j]+1;
-				} else {
-					if(str1.charAt(j-1) == str2.charAt(i-1)) {
-						arr[i][j] = arr[i-1][j-1];
+	public static int getNumWays(int n) {
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 6; j++) {
+				if(i == 0) {
+					if( j == 0) {
+						arr[i][j] = 1;
+					} else if((j)%dinno[i] == 0) {
+						arr[i][j] = 1;
 					} else {
-						arr[i][j] = minNum(arr[i-1][j], arr[i-1][j-1], arr[i][j-1]) + 1;
+						arr[i][j] = 0;
+					}
+				} else if(j == 0) {
+					arr[i][j] = 1;
+				} else {
+					if(j < dinno[i]) {
+						arr[i][j] = arr[i-1][j];
+					} else {
+						arr[i][j] = arr[i-1][j] + arr[i][j-dinno[i]];
 					}
 				}
 			}
 		}
-		dist = arr[l1][l2];
-		return dist;
-	}
-	
-	public static int minNum(int a, int b, int c) {
-		if(a<b) {
-			if(a<c) return a;
-			else return c;
-		} else {
-			if(b < c) return b;
-			else return c;
-		}
+		
+		return arr[2][5];
 	}
 
 }
-
